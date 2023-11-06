3aa23ebe46f30a8263a6a2f504e570a0e73d331b
shodon <about@shodon.ru>
2023-11-06 12:12:33 +0700

    updated breadcrumbs

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const router = require("./routes/index.js");
 const errorHandler = require("./error/errorHandler.js");
 const checkToken = require("./middlewares/checkToken.js");
@@ -8,6 +9,7 @@ const checkToken = require("./middlewares/checkToken.js");
 const app = express();
 const PORT = +(process.env.PORT ?? 9999);
 
+app.use(express.static(path.resolve(__dirname, "static")));
 app.use(checkToken);
 app.use(cors());
 app.use(express.json());
