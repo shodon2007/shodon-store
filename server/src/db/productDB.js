43cc2beac084fd9f6e822e474ddcdf281e381103
shodon <about@shodon.ru>
2024-02-19 16:27:02 +0700

    final fix

--- server/src/db/productDB.js
+++ server/src/db/productDB.js
@@ -4,38 +4,45 @@ class ProductDB extends Database {
     async getAll(type) {
         let devices = await this.query(
             `
-            SELECT 
-    subquery.*,
-    JSON_PARSE(subquery.attributes) AS attributes_parsed,
-    subquery.reviews,
-    subquery.rate
-FROM (
-    SELECT
-        d.*,
-        JSON_ARRAYAGG(JSON_OBJECT('id', a.id, 'title', a.title, 'description', a.description)) AS attributes,
-        (
-            SELECT
-                COUNT(*)
+                SELECT
+                d.*,
+                JSON_ARRAYAGG(JSON_OBJECT('id', a.id, 'title', a.title, 'description', a.description)) AS attributes,
+                (
+                    SELECT
+                        COUNT(*)
+                    FROM
+                        review r
+                    WHERE
+                        r.device_id = d.id
+                ) AS reviews,
+                (
+                    SELECT
+                        ROUND(AVG(rate),2)
+                    FROM
+                        review r
+                    WHERE
+                        r.device_id = d.id
+                ) AS rate
             FROM
-                review r
+                device d
+            LEFT JOIN
+                attribute a ON a.device_id = d.id
+            INNER JOIN
+                type ON d.type_id = type.id
             WHERE
-                r.device_id = d.id
-        ) AS reviews,
-        (
-            SELECT
-                ROUND(AVG(rate),2)
-            FROM
-                review r
-            WHERE
-                r.device_id = d.id
-        ) AS rate
-    FROM
-        device d
-    LEFT JOIN
-        attribute a ON a.devi
-
+                type.name = ?
+            GROUP BY
+                d.id, d.name
                 `, type
         );
+
+        devices = devices.map(el => {
+            if (el.attributes === 'string') {
+                el.attributes = JSON.parse(el.attributes);
+            }
+
+            return el;
+        })
 
         return devices;
     }
