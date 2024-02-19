82b5ee0c3c2970f5725578bb8ee55e92401f6190
shodon <about@shodon.ru>
2024-02-19 16:11:51 +0700

    fixed server bug

--- server/src/db/productDB.js
+++ server/src/db/productDB.js
@@ -5,34 +5,35 @@ class ProductDB extends Database {
         let devices = await this.query(
             `
             SELECT
-                d.*,
-                JSON_ARRAYAGG(JSON_OBJECT('id', a.id, 'title', a.title, 'description', a.description)) AS attributes,
-                (
-                    SELECT
-                        COUNT(*)
-                    FROM
-                        review r
-                    WHERE
-                        r.device_id = d.id
-                ) AS reviews,
-                (
-                    SELECT
-                        ROUND(AVG(rate),2)
-                    FROM
-                        review r
-                    WHERE
-                        r.device_id = d.id
-                ) AS rate
-            FROM
-                device d
-            LEFT JOIN
-                attribute a ON a.device_id = d.id
-            INNER JOIN
-                type ON d.type_id = type.id
-            WHERE
-                type.name = ?
-            GROUP BY
-                d.id, d.name
+    d.*,
+    JSON_ARRAYAGG(JSON_OBJECT('id', a.id, 'title', a.title, 'description', a.description)) AS attributes,
+    (
+        SELECT
+            COUNT(*)
+        FROM
+            review r
+        WHERE
+            r.device_id = d.id
+    ) AS reviews,
+    (
+        SELECT
+            ROUND(AVG(rate),2)
+        FROM
+            review r
+        WHERE
+            r.device_id = d.id
+    ) AS rate
+FROM
+    device d
+LEFT JOIN
+    attribute a ON a.device_id = d.id
+INNER JOIN
+    type ON d.type_id = type.id
+WHERE
+    type.name = ?
+GROUP BY
+    d.id, d.name
+
                 `, type
         );
 
