be0c4a3f5aded11fdbe675b4c74c2fa3c25656d2
shodon <about@shodon.ru>
2024-02-19 16:13:45 +0700

    fix fix

--- server/src/db/productDB.js
+++ server/src/db/productDB.js
@@ -4,35 +4,43 @@ class ProductDB extends Database {
     async getAll(type) {
         let devices = await this.query(
             `
+            SELECT 
+            d.*,
+            JSON_PARSE(attributes) AS attributes_parsed,
+            reviews,
+            rate
+        FROM (
             SELECT
-    d.*,
-    JSON_ARRAYAGG(JSON_OBJECT('id', a.id, 'title', a.title, 'description', a.description)) AS attributes,
-    (
-        SELECT
-            COUNT(*)
-        FROM
-            review r
-        WHERE
-            r.device_id = d.id
-    ) AS reviews,
-    (
-        SELECT
-            ROUND(AVG(rate),2)
-        FROM
-            review r
-        WHERE
-            r.device_id = d.id
-    ) AS rate
-FROM
-    device d
-LEFT JOIN
-    attribute a ON a.device_id = d.id
-INNER JOIN
-    type ON d.type_id = type.id
-WHERE
-    type.name = ?
-GROUP BY
-    d.id, d.name
+                d.*,
+                JSON_ARRAYAGG(JSON_OBJECT('id', a.id, 'title', a.title, 'description', a.description)) AS attributes,
+                (
+                    SELECT
+                        COUNT(*)
+                    FROM
+                        review r
+                    WHERE
+                        r.device_id = d.id
+                ) AS reviews,
+                (
+                    SELECT
+                        ROUND(AVG(rate),2)
+                    FROM
+                        review r
+                    WHERE
+                        r.device_id = d.id
+                ) AS rate
+            FROM
+                device d
+            LEFT JOIN
+                attribute a ON a.device_id = d.id
+            INNER JOIN
+                type ON d.type_id = type.id
+            WHERE
+                type.name = ?
+            GROUP BY
+                d.id, d.name
+        ) AS subquery;
+        
 
                 `, type
         );
