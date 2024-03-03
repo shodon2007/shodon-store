b94d9caf769b91f9bb439427d6d1ae01d812788f
shodon <about@shodon.ru>
2024-03-03 18:27:40 +0700

    fix bug in filterDB

--- server/src/db/filterDB.js
+++ server/src/db/filterDB.js
@@ -3,12 +3,14 @@ const Database = require(".");
 class FilterDB extends Database {
     async getFilter(type) {
         let data = await this.query(
-            `SELECT attribute.* FROM attribute 
+            `SELECT DISTINCT attribute.title, attribute.description FROM attribute 
             INNER JOIN device ON attribute.device_id = device.id
             INNER JOIN type ON device.type_id = type.id
             WHERE type.name = ?`,
             type
         );
+
+        console.log(data);
 
         const newData = {};
 
